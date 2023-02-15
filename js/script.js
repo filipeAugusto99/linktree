d564e4e32eab81c6ac63b9ef71244cd7371fc23b
Filipe Augusto <filipecordeiro213@gmail.com>
2023-02-15 15:26:18 -0300

    some modifies in javascript

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-const button = document.querySelector('#button_home')
+// Seleciona todos os links internos que levam a seções na pág
+const links = document.querySelectorAll('a[href^="#"]')
 
-button.onclick = function() {
-  const sectionHome = document.querySelector('#home')
-  sectionHome.scrollIntoView({behavior: 'smooth'})
-}
+// Adiciona um evento de click para cada link
+links.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault(); //impedir que o link execute o comportamento padrao
+    const section = document.querySelector(this.hash)
+    const top = section.offsetTop
+
+    window.scroll({
+      top,
+      behavior: 'smooth'
+    })
+  })
+})
